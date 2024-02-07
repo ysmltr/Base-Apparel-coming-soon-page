809dbdb176ab0368acaa1eebcb51dab3b6ce0981
ysmltr <yesimltr1@gmail.com>
2024-02-07 13:25:35 +0300

    Remove unused CSS file and update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
-
